a26b940320238d97e2c078910e790b96ed13734c
Luke Gessler <lukegessler@gmail.com>
2021-02-07 23:20:57 -0500

    configurable data helper name for test

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python main.py \
 	--eval \
+	--train_dir "../data/train_dir" \
 	--eval_dir "../data/test_dir/" \
 	--model_name "test_model.pt" \
 	--model_type 0 \
--- src/test_gum.sh
+++ src/test_gum.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python main.py \
 	--eval \
+	--train_dir "../data/gum_train/" \
 	--eval_dir "../data/gum_test/" \
 	--model_name "gum_model.pt" \
 	--model_type 0 \
